028fc3bcb0c3048b2caf2c3fc6e3cff091668b50
TajRask <roqayah1994@gmail.com>
2022-12-31 18:59:00 +0300

    js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
+const addBtn=document.getElementById('add-btn');
+const delBtn=document.getElementById('del-btn');
+const editBtn=document.getElementById('edit-btn');
+const text = document.getElementById('text-area')
 
+
+
+function addNote();
+
+
+
+function deleteNote();
+
+
+function editNote();
+
+
+function addToLS();
+
+
+function removeFromLS();
