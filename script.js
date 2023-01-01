03a821ec4c5333d122318bfc624dc077165f7d4e
TajRask <roqayah1994@gmail.com>
2023-01-01 14:20:34 +0300

    happy new year

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
-const addBtn=document.getElementById('add-btn');
-const delBtn=document.getElementById('del-btn');
-const editBtn=document.getElementById('edit-btn');
-const text = document.getElementById('text-area')
+const addBtn = document.getElementById('add-btn');
+const delBtn = document.getElementById('del-btn');
+const editBtn = document.getElementById('edit-btn');
+const textArea = document.getElementById('text-area');
+const noteBody = document.getElementById('note-body')
 
 
 
-function addNote();
-
-
+function addNote(note) {
+    const notes = document.createElement('div')
+    notes.classList.add('note-body')
+    notes.innerHTML = `
+    
+    <div class="note-body" id="note-body">
+    <textarea id="text-area">${textArea}</textarea>
+    </div>
+    
+    `
+}
 
 function deleteNote();
 
