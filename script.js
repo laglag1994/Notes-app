82073bde5f315a684fbadcb01f4b691acce7d64c
TajRask <roqayah1994@gmail.com>
2023-01-03 05:43:22 +0300

    fmdsafmds

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const editBtn=document.querySelector('edit-btn')
+const notesContainer=document.getElementById('notes-page');
+const addNoteBtn=notesContainer.querySelector('.add-note')
+
+//retrive all the notes from LS
+
+function getNotes(){
+return JSON.parse(localStorage.getItem('notes')|| '[]');
+}
+console.log(getNotes)
+
+
+//save the new notes to LS
+function saveNotes (notes){
+     
+}
+
+//create new note from HTML
+function createNoteElement(id, noteContent){
+
+}
+
+//add a new note 
+function addNote (){
+
+}
+
+
+//udating notes
+function updateNote(id, newNoteContent){
+
+}
+
+//deleting a note
+function deleteNote (id, element){
+
+}
