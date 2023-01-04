ddcf4a3f451af2d58f1c596b81afe73ca69cba14
TajRask <roqayah1994@gmail.com>
2023-01-04 19:09:08 +0300

    textArea

--- script.js
+++ script.js
@@ -1,50 +1,66 @@
-const notesContainer=document.getElementById('notes-page');
+const notesContainer = document.getElementById('notes-page');
 
 
-const addNoteBtn=notesContainer.querySelector('.add-note');
-const editBtn=notesContainer.querySelector('.edit-btn');
-const delBtn=notesContainer.querySelector('.del-btn');
+const addNoteBtn = notesContainer.querySelector('.add-note');
+const editBtn = notesContainer.querySelector('.edit-btn');
+const delBtn = notesContainer.querySelector('.del-btn');
+
+
+//get notes and display
+getNotes().forEach(note => {
+    const noteEl = createNoteElement(note.id, note.noteContent);
+    
+});
+
 
 //retrive all the notes from LS
-console.log(getNotes)
+function getNotes() {
+    return JSON.parse(localStorage.getItem('note') || '[]');
 
-
-function getNotes(){
-return JSON.parse(localStorage.getItem('note')|| '[]');
- 
 }
 
 
 //save the new notes to LS
-function saveNotes (notes){
+function saveNotes(notes) {
     localStorage.setItem('note', JSON.stringify(notes))
 }
 
 //create new note from HTML
-function createNoteElement(id, noteContent){
-const element=document.createElement('textarea');
-element.classList.add('note-body');
-element.value=noteContent;
+function createNoteElement(id, noteContent) {
+    const element = document.createElement('textarea');
 
-element.addEventListener('edit',()=>{
-updateNote(id, element.value)
-});
 
-return element;
+    
+
+    element.classList.add('note-body');
+    element.value = noteContent;
+
+    editBtn.addEventListener('click', () => {
+        updateNote(id, element.value)
+    });
+
+    delBtn.addEventListener('click', () => {
+        deleteNote(id, element);
+    })
+
+    return element;
 }
 
 //add a new note 
-function addNote (){
+function addNote() {
 
 }
 
 
 //udating notes
-function updateNote(id, newNoteContent){
+function updateNote(id, newNoteContent) {
+    console.log('update note')
+    console.log(id, newNoteContent)
 
 }
 
 //deleting a note
-function deleteNote (id, element){
-
+function deleteNote(id, element) {
+    console.log('delete note')
+    console.log(id, newNoteContent)
 }
