4ea418511990bc17d73482ff8a8ba0934cfc6c20
TajRask <roqayah1994@gmail.com>
2023-01-04 17:38:19 +0300

    LS

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const notesContainer=document.getElementById('notes-page');
-const addNoteBtn=notesContainer.querySelector('.add-note')
+
+
+const addNoteBtn=notesContainer.querySelector('.add-note');
+const editBtn=notesContainer.querySelector('.edit-btn');
+const delBtn=notesContainer.querySelector('.del-btn');
 
 //retrive all the notes from LS
+console.log(getNotes)
+
 
 function getNotes(){
-return JSON.parse(localStorage.getItem('notes')|| '[]');
+return JSON.parse(localStorage.getItem('note')|| '[]');
+ 
 }
 
 
 //save the new notes to LS
 function saveNotes (notes){
-     
+    localStorage.setItem('note', JSON.stringify(notes))
 }
 
 //create new note from HTML
 function createNoteElement(id, noteContent){
+const element=document.createElement('textarea');
+element.classList.add('note-body');
+element.value=noteContent;
 
+element.addEventListener('edit',()=>{
+updateNote(id, element.value)
+});
+
+return element;
 }
 
 //add a new note 
